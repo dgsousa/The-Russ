ca44211126db8f1b5f54eca36cf8b8d944f384b1
Russ DuBrock <rdubrock@marketo.com>
2018-07-03 14:48:09 -0700

    adds data for fishies

---
+++ src/data/fishData.js
@@ -0,0 +1,36 @@
+const fishData = [
+  {
+    id: 1,
+    name: 'Great white shark',
+    color: 'white',
+    length: '21 ft',
+    weight: '2400 lbs',
+    image: '/images/208px-White_shark.jpg',
+  },
+  {
+    id: 2,
+    name: 'Sunfish',
+    color: 'gray',
+    length: '10.5 ft',
+    weight: '2,205 lbs',
+    image: '/images/178px-Sunfish2.jpg',
+  },
+  {
+    id: 3,
+    name: 'Manta ray',
+    color: 'Black',
+    length: '23 ft',
+    weight: '2,980 lbs',
+    image: '/images/218px-Dharavandhoo_Thila_-_Manata_Black_Pearl.jpeg',
+  },
+  {
+    id: 4,
+    name: 'Flying fish',
+    color: 'Silver',
+    length: '12.6 in',
+    weight: '3.5 lbs',
+    image: '/images/Cheilopogon_melanurusPCCA20070623-3956B.jpg',
+  }
+]
+
+export default fishData;
