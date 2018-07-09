dbb7902b8990c92634b2f245165e458af4c19492
Russ DuBrock <rdubrock@marketo.com>
2018-07-09 13:59:02 -0700

    removes bug

--- src/components/Drawer/DrawerList.js
+++ src/components/Drawer/DrawerList.js
@@ -1,31 +1,20 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import { drawerItemSelectedThunk } from '../../thunks/componentThunks.js';
-
-const mapDispatchToProps = {
-  selectItem: drawerItemSelectedThunk,
-};
 
 class DrawerList extends Component {
-  buildItemClick = (selection) => () => {
-    const { selectItem } = this.props;
-    selectItem(selection);
-  }
-
   render() {
     return (
       <List>
         <ListItem
-          onClick={ this.buildItemClick('birds') }
+          onClick={ () => console.log('TO DO') }
           button
         >
           <ListItemText primary='Birds' />
         </ListItem>
         <ListItem
-          onClick={ this.buildItemClick('fish') }
+          onClick={ () => console.log('TO DO') }
           button
         >
           <ListItemText primary='Fish' />
@@ -35,4 +24,4 @@ class DrawerList extends Component {
   }
 }
 
-export default connect(null, mapDispatchToProps)(DrawerList);
+export default DrawerList;
