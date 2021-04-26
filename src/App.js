3097f991de608ad54a25343ec162879e70a20357
Ash Provost <provost@adobe.com>
2021-04-26 16:12:24 -0700

    convert app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Header from './components/Header/Header.js';
 import AppBody from './components/AppBody/AppBody.js';
 import DevToolsWrapper from './components/DevTools/DevToolsWrapper.js';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <div className='App'>
-          <Header />
-          <AppBody />
-        </div>
-        <div>
-          <DevToolsWrapper />
-        </div>
+export default function() {
+  return (
+    <div>
+      <div className='App'>
+        <Header />
+        <AppBody />
       </div>
-    );
-  }
+      <div>
+        <DevToolsWrapper />
+      </div>
+    </div>
+  );
 }
-
-export default App;
